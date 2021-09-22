a74ad93803b56d03958f3fcb0cc729c79a4e0a0d
ridaarif98 <ridaarif16@gmail.com>
2021-09-22 19:21:49 +0200

    Add event to handle editing

--- src/functions/TodoItem.js
+++ src/functions/TodoItem.js
@@ -1,21 +1,58 @@
 import React from 'react';
 
 class TodoItem extends React.Component {
+  state = {
+    editing: false,
+  };
+
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    });
+  };
+  handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      this.setState({ editing: false });
+    }
+  };
+
   render() {
+    const viewMode = {};
+    const editMode = {};
+
+    if (this.state.editing) {
+      viewMode.display = 'none';
+    } else {
+      editMode.display = 'none';
+    }
     return (
       <li className="item">
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input
+            type="checkbox"
+            className="checkbox"
+            checked={this.props.todo.completed}
+            onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+          />
+          <button
+            onClick={() => this.props.deleteTodoProps(this.props.todo.id)}
+          >
+            Delete
+          </button>
+          <span className={this.props.todo.completed ? 'completedStyle' : null}>
+            {this.props.todo.title}
+          </span>
+        </div>
         <input
-          type="checkbox"
-          className="checkbox"
-          checked={this.props.todo.completed}
-          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+          type="text"
+          className="input-text"
+          style={editMode}
+          value={this.props.todo.title}
+          onChange={(e) => {
+            this.props.setUpdate(e.target.value, this.props.todo.id);
+          }}
+          onKeyDown={this.handleUpdatedDone}
         />
-        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-          Delete
-        </button>
-        <span className={this.props.todo.completed ? 'completedStyle' : null}>
-          {this.props.todo.title}
-        </span>
       </li>
     );
   }
