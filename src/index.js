05f063d888e24cb121e59ae1e5ed76afc58debdf
ridaarif98 <ridaarif16@gmail.com>
2021-09-22 11:30:59 +0200

    Create list and add event on checkbox

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TodoContainer from './functions/TodoContainer'
 
 ReactDOM.render(
-  <React.StrictMode>
-
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+<React.StrictMode>
+  <TodoContainer />
+</React.StrictMode>, 
+document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
